769d2170517ccc73bd8fe48f7b496a7465f17423
user name <kalathursuryaprakash2002@gmail.com>
2023-03-16 09:10:31 +0530

    first commit

---
+++ src/assets/environments/environment.ts
@@ -0,0 +1,9 @@
+export const environment = {
+  production: true,
+  weatherApiBaseUrl: 'https://weather-by-api-ninjas.p.rapidapi.com/v1/weather',
+  XRapidAPIHostHeaderName: 'X-RapidAPI-Host',
+  XRapidAPIHostHeaderValue: 'weather-by-api-ninjas.p.rapidapi.com',
+  XRapidAPIKeyHeaderName: 'X-RapidAPI-Key',
+  XRapidAPIKeyHeaderValue: 'ae374021d9mshe9027cb32d660c6p14c55ajsn348270248ebc'
+
+};
